c6914e3858298ae9e7942248ad57ee8fd2c070b6
Jamie Morrissey <jmo1344@wgu.edu>
2024-12-07 00:29:54 -0800

    added files

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -9,4 +9,31 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'angular';
+
+
+  setId:string = "";
+  setCapital:string = "";
+  setRegion: string = "";
+  setIncomelevel: string = "";
+  setLatitude: string = "";
+  setLongitude: string = "";
+
+fromIDChoose(id: string) {
+  this.setId = id;
+}
+fromCapitalChoose(capital: string) {
+  this.setCapital = capital;
+}
+fromRegionChoose(region: string) {
+  this.setRegion = region;
+}
+fromIncomeChoose(income: string) {
+  this.setIncomelevel = income;
+}
+fromLatitudeChoose(latitude: string) {
+  this.setLatitude = latitude;
+}
+fromLongitudeChoose(longitude: string) {
+  this.setLongitude = longitude;
+}
 }
---
+++ angular/src/app/country/country.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country',
+  imports: [],
+  templateUrl: './country.component.html',
+  styleUrl: './country.component.css'
+})
+export class CountryComponent {
+  @Input() nationId!: string;
+  @Input() nationCapital!: string:
+  @Input() nationRegion!: string;
+  @Input() nationIncome!: string;
+  @Input() nationLatitude!: string;
+  @Input() nationLongitude!: string;
+
+  constructor() { }
+
+  onInit(): void { }
+}
+
+}
