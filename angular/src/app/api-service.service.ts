fe1e6c83def75d5c129721f103e2251c0c17f356
Jamie Morrissey <jmo1344@wgu.edu>
2024-12-07 02:03:49 -0800

    added nationid var

--- angular/src/app/api-service.service.ts
+++ angular/src/app/api-service.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCountryData(countryID: string): Observable<User> {
+    const my_url = `http://api.worldbank.org/v2/country/${nationId}?format=json`;
+
+    return this.http.get<any>(my_url);
+}
 }
